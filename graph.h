539386c2e00fdf86ad13716e2e6c6b05ab8ecfff
YEADOS <nicholasyeadon@gmail.com>
2025-04-26 17:41:39 +1000

    first commit

---
+++ graph.h
@@ -0,0 +1,21 @@
+#ifndef GRAPH_H
+#define GRAPHS_H 
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+
+class Graph {
+    private:
+        std::vector<std::vector<int>> puzzle;
+
+    public: 
+
+        Graph();
+
+
+};
+
+#endif
